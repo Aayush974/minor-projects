fca23dd5c02c5fc300677826697ccb7b669370d5
Aayush <chandayush005@gmail.com>
2024-12-05 17:50:15 +0530

    game logic defined

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,14 +1,3 @@
-// *****code to change the css to display the cross and circle*****
-// const crossContainer = document.getElementById('11');
-// const cross = crossContainer.children[0];
-
-// cross.addEventListener('click',e=>{
-//   cross.children[0].style.transform = "rotate(45deg)";
-  
-//   cross.children[1].style.transform = "rotate(-45deg)";
-  
-// })
-
 //variables initialization
 
 let turnNum = 0;
@@ -38,16 +27,23 @@ for(let i=0;i<rows.length;i++){
     if(player1Turn){
       player1Arr.push(Number(e.target.id));// keeping track of player1's moves
       sortArr(player1Arr);
-
-    }else if(player2Turn){
+      let cross = e.target.children[0];
+      cross.style.display = "initial";
+      cross.children[0].style.transform = "rotate(45deg)";
+      cross.children[1].style.transform = "rotate(-45deg)";
+      if(turnNum>4){
+        if(isWinning(player1Arr)) console.log("player 1 wins");
+      }
+    }
+    else if(player2Turn){
       player2Arr.push(Number(e.target.id));// keeping track of player2's moves
       sortArr(player2Arr);
-    }
-  
-    //now to check if someone has won in this turn
-
-    if(turnNum>=5){ // winning before turn no. 5 is impossible
-      //some function which takes the sorted player arrays and finds if someone is winning based on the logic defined inside it
+      let circle = e.target.children[1];
+      circle.style.display = "initial";
+      
+      if(turnNum>4){
+        if(isWinning(player2Arr)) console.log("player 2 wins");
+      }
     }
 
   })
@@ -56,6 +52,65 @@ for(let i=0;i<rows.length;i++){
 //function to sort the player arrays
 
 function sortArr(arr){
- 
+ let temp;
+ for(let i=0;i<arr.length;i++){
+
+  for(let j=0;j<(arr.length-1);j++){
+    if(arr[j]>arr[j+1]){
+       temp=arr[j+1];
+       arr[j+1]=arr[j];
+       arr[j]=temp;
+    }
+  }
+
+ }
+
 }
 
+//function to search thru the player arrays
+
+function binarySearch(targetEl,arr){
+  let high,low,mid;
+  low = 0;
+  high = arr.length - 1;
+  mid = Math.floor((low+high)/2);
+  
+  while(low<=high){
+    mid = Math.floor((low+high)/2);
+    if(arr[mid]==targetEl){
+      return true;
+    }
+    else{
+      if(arr[mid]<targetEl){
+        low = mid+1;
+      }else if(arr[mid]>targetEl){
+        high = mid-1;
+      }
+    }
+
+  }
+  return false;
+}
+
+//function for checking if a player has won 
+
+function isWinning(arr){
+  let currentEl;
+
+  for(let i=0;i<arr.length;i++){
+     currentEl = arr[i];
+
+     if(arr[i+1]==arr[i]+1 && arr[i+2]==arr[i]+2){//row wise win
+      return 1;
+     }
+     else if(binarySearch(arr[i]+10,arr) && binarySearch(arr[i]+20,arr)){//column wise win
+      return 1;
+     }
+     else if(binarySearch(arr[i]+11,arr) && binarySearch(arr[i]+22,arr)){//diagonal wise win (this most likely does not account for the 2nd possible diagonal orientation)
+      return 1;
+     }
+     else{return 0;}
+
+  }
+
+}
